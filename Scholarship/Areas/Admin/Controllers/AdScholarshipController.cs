2e6d18bb03330956689c24bad9ccc6e3ba895110
Jay shah <shah.jay60@yahoo.com>
2021-09-05 11:14:14 +0530

    Commit schoarship form

---
+++ Scholarship/Areas/Admin/Controllers/AdScholarshipController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scholarship.Areas.Admin.Controllers
+{
+    public class AdScholarshipController : Controller
+    {
+        // GET: Admin/AdScholarship
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
