1b160d219a1ef1d60e9bce3a4ddcfe589ca0aaa2
Jay shah <shah.jay60@yahoo.com>
2021-09-17 13:37:40 +0530

    Student details and list to admin side

---
+++ Scholarship/Areas/Admin/Controllers/AdStudentDetailsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scholarship.Areas.Admin.Controllers
+{
+    public class AdStudentDetailsController : Controller
+    {
+        // GET: Admin/AdStudentDetails
+        ScholarshipEntities entity = new ScholarshipEntities();
+
+        public ActionResult Index()
+        {
+            var model = entity.tblStudentDetails.ToList();
+            return View(model);
+        }
+        public ActionResult Detail(int id)
+        {
+            var model = entity.tblStudentDetails.ToList().Where(x => x.Id == id).FirstOrDefault() ;
+            return View(model);
+        }
+    }
+}
