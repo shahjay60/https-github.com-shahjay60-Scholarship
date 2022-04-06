8eefbf64d866136b545342332ba7e6290f839171
Jay shah <shah.jay60@yahoo.com>
2022-04-06 23:21:04 +0530

    Commit exam page with instruction

---
+++ Scholarship/Controllers/ExamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scholarship.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ExamDetails()
+        {
+            return View();
+        }   
+    }
+}
